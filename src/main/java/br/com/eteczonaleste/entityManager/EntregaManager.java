3cee421fad1dbddc44ff058a70c2d6b2f03d22a0
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-05-04 22:41:16 -0300

    ao invest de consultar se o aluno ja almocou no banco, a gente consulta num arq txt (falta testar)

--- src/main/java/br/com/eteczonaleste/entityManager/EntregaManager.java
+++ src/main/java/br/com/eteczonaleste/entityManager/EntregaManager.java
@@ -5,6 +5,7 @@ import br.com.eteczonaleste.entity.Entregas;
 import br.com.eteczonaleste.entity.Funcionarios;
 import br.com.eteczonaleste.view.Principal;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
@@ -23,88 +24,104 @@ import java.util.logging.Logger;
 
 public class EntregaManager {
 
+	public EntregaManager() {
+	}
 
-    public EntregaManager() {       
-    }
+	public void insert(Entregas entrega) {
+		try {
+			Principal.em.getTransaction().begin();
+			Principal.em.persist(entrega);
+			Principal.registraEntregaNoArquivoDeEntregasDoDia(entrega);
+			Principal.em.getTransaction().commit();
+		} catch (Exception e) {
 
-    public void insert(Entregas entrega) {
-        Principal.em.getTransaction().begin();
-        Principal.em.persist(entrega);
-        Principal.em.getTransaction().commit();
-    }
-    
-    public boolean verificaSeOAlunoJaAlmocou(Alunos nra){
-    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date dta = new Date();
-        java.sql.Date sqlDate = new java.sql.Date(dta.getTime());
-                
-        Query entregaPesq = Principal.em.createQuery("SELECT count(e) FROM Entregas e WHERE e.ra = :nra AND e.data_retirada = :data", Entregas.class);
-               entregaPesq.setParameter("data", sqlDate);
-               entregaPesq.setParameter("nra", nra);
+		}
+	}
 
-        //ID, DATA_RETIRADA, HORA_RETIRADA, ra, ID_FUNCIONARIOS_ID       
-        int qtdeAlmocos = Integer.parseInt(entregaPesq.getResultList().get(0).toString());
-        boolean result = qtdeAlmocos == 0 ? false : true;
-        System.out.println("Ja amocou ? "+result);
-        return result;
-    }
+	public boolean verificaSeOAlunoJaAlmocou(Alunos nra) {
 
-    public List<Entregas> findSelectAll(String data) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date dta = null;
+//		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//		Date dta = new Date();
+//		java.sql.Date sqlDate = new java.sql.Date(dta.getTime());
 
-        try {
-            dta = formatter.parse(data);
-        } catch (ParseException ex) {
-            Logger.getLogger(EntregaManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//		Query entregaPesq = Principal.em.createQuery(
+//				"SELECT count(e) FROM Entregas e WHERE e.ra = :nra AND e.data_retirada = :data", Entregas.class);
+//		entregaPesq.setParameter("data", sqlDate);
+//		entregaPesq.setParameter("nra", nra);
+		// ID, DATA_RETIRADA, HORA_RETIRADA, ra, ID_FUNCIONARIOS_ID
+//		int qtdeAlmocos = Integer.parseInt(entregaPesq.getResultList().get(0).toString());
+//		boolean result = qtdeAlmocos == 0 ? false : true;
+//		System.out.println("Ja amocou ? " + result);
+		
+		boolean result = false;
+		String line;
+		try {
+			while ((line = Principal.bufferedReader4ArqEntregasDoDia.readLine()) != null) {
+			    if(Integer.parseInt(line.split(";")[0]) == nra.getRa()) return true; 
+			    else return false;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
 
-        
-        Query entregaPesq = Principal.em.createQuery("SELECT e FROM Entregas e WHERE e.data_retirada = :data", Entregas.class);
-                entregaPesq.setParameter("data", dta);
+	public List<Entregas> findSelectAll(String data) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date dta = null;
 
-        //ID, DATA_RETIRADA, HORA_RETIRADA, ra, ID_FUNCIONARIOS_ID
-        
-        return entregaPesq.getResultList();
-    }
+		try {
+			dta = formatter.parse(data);
+		} catch (ParseException ex) {
+			Logger.getLogger(EntregaManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-    public long findSelectTotal(String data) {
+		Query entregaPesq = Principal.em.createQuery("SELECT e FROM Entregas e WHERE e.data_retirada = :data",
+				Entregas.class);
+		entregaPesq.setParameter("data", dta);
 
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date dta = null;
+		// ID, DATA_RETIRADA, HORA_RETIRADA, ra, ID_FUNCIONARIOS_ID
 
-        try {
-            dta = formatter.parse(data);
-        } catch (ParseException ex) {
-            Logger.getLogger(EntregaManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		return entregaPesq.getResultList();
+	}
 
-        Query entregaPesq = (Query) Principal.em.createQuery("SELECT COUNT(a) FROM Entregas a WHERE a.data_retirada = :data")
-                .setParameter("data", dta);
+	public long findSelectTotal(String data) {
 
-        return (Long) entregaPesq.getSingleResult();
-    }
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date dta = null;
 
-    public void delete(Entregas entrega) {
-        Principal.em.getTransaction().begin();
-        Principal.em.remove(entrega);
-        Principal.em.getTransaction().commit();        
-    }
+		try {
+			dta = formatter.parse(data);
+		} catch (ParseException ex) {
+			Logger.getLogger(EntregaManager.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-    public Collection<Entregas> execJpql(String jpql, Map<String,Object> parameters){
-        List<Entregas> resultList = new ArrayList<Entregas>();
-        
-        Query qry = (Query) Principal.em.createQuery(jpql);
-        
-        if(!parameters.isEmpty()){
-            Iterator<Map.Entry<String, Object>> iterator = parameters.entrySet().iterator();
-            while(iterator.hasNext()){
-                Map.Entry<String, Object> next = iterator.next();
-                qry.setParameter(next.getKey(), next.getValue());
-            }
-        }
-        
-        resultList = qry.getResultList();
-        return resultList;
-    }
-}//class
+		Query entregaPesq = (Query) Principal.em
+				.createQuery("SELECT COUNT(a) FROM Entregas a WHERE a.data_retirada = :data").setParameter("data", dta);
+
+		return (Long) entregaPesq.getSingleResult();
+	}
+
+	public void delete(Entregas entrega) {
+		Principal.em.getTransaction().begin();
+		Principal.em.remove(entrega);
+		Principal.em.getTransaction().commit();
+	}
+
+	public Collection<Entregas> execJpql(String jpql, Map<String, Object> parameters) {
+		List<Entregas> resultList = new ArrayList<Entregas>();
+
+		Query qry = (Query) Principal.em.createQuery(jpql);
+
+		if (!parameters.isEmpty()) {
+			Iterator<Map.Entry<String, Object>> iterator = parameters.entrySet().iterator();
+			while (iterator.hasNext()) {
+				Map.Entry<String, Object> next = iterator.next();
+				qry.setParameter(next.getKey(), next.getValue());
+			}
+		}
+
+		resultList = qry.getResultList();
+		return resultList;
+	}
+}// class
--- src/main/java/br/com/eteczonaleste/view/Principal.java
+++ src/main/java/br/com/eteczonaleste/view/Principal.java
@@ -5,6 +5,17 @@ import java.awt.Container;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -18,161 +29,230 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 import br.com.eteczonaleste.entity.Alunos;
+import br.com.eteczonaleste.entity.Entregas;
 import br.com.eteczonaleste.entityManager.AlunosManager;
 
 public class Principal extends JFrame {
 
 	static EntityManagerFactory emf = Persistence.createEntityManagerFactory("e211PU");
-    static public EntityManager em = emf.createEntityManager();
+	static public EntityManager em = emf.createEntityManager();
 
-    private JMenu menuCadastrar = new JMenu("Cadastrar");
-    //private JMenu menuEditar = new JMenu("Editar");
-    private JMenu menuRelatorio = new JMenu("Relatório");
-    private JMenu menuEntrega = new JMenu("Entrega");
+	static SimpleDateFormat formatadorDeData = new SimpleDateFormat("dd-MM-yyyy");
+	static SimpleDateFormat formatadorDeHoras = new SimpleDateFormat("HH:mm:ss");
+	static File arqDeEntregasDoDia = Principal.criaArquivoDeEntregasDoDia();
+	static PrintStream fileStream4ArqDeEntregasDoDia = Principal.escritorDeLinhaDoArquivoDeEntregas();
 
-    private JMenuItem menuItemCadastrarRefeicaoManual = new JMenuItem("Refeição");
-    private JMenuItem menuItemCadastrarAluno = new JMenuItem("Aluno");
-    private JMenuItem menuItemCadastrarBiometria = new JMenuItem("Biometria");
-    private JMenuItem menuItemCadastrarFuncionario = new JMenuItem("Funcionário");
-    private JMenuItem menuItemRelatorioFuncionario = new JMenuItem("Funcionários");
-    private JMenuItem menuItemRelatorioAluno = new JMenuItem("Alunos");
-    private JMenuItem menuItemRelatorioEntrega = new JMenuItem("Entrega");
+	static FileReader fileReader4ArqEntregasDoDia = criaReaderDoArquivoDeEntregasDoDia();
+	public static BufferedReader bufferedReader4ArqEntregasDoDia = new BufferedReader(fileReader4ArqEntregasDoDia);
 
-    private JMenuItem menuItemSobre = new JMenuItem("Sobre");
-    private JMenuItem menuItemFechar = new JMenuItem("Fechar");
 
-    private JMenuItem menuItemEntrega = new JMenuItem("Merenda Escolar");
+	
+	
+	private JMenu menuCadastrar = new JMenu("Cadastrar");
+	// private JMenu menuEditar = new JMenu("Editar");
+	private JMenu menuRelatorio = new JMenu("Relatório");
+	private JMenu menuEntrega = new JMenu("Entrega");
 
-    private JMenuBar menuBar = new JMenuBar();
+	private JMenuItem menuItemCadastrarRefeicaoManual = new JMenuItem("Refeição");
+	private JMenuItem menuItemCadastrarAluno = new JMenuItem("Aluno");
+	private JMenuItem menuItemCadastrarBiometria = new JMenuItem("Biometria");
+	private JMenuItem menuItemCadastrarFuncionario = new JMenuItem("Funcionário");
+	private JMenuItem menuItemRelatorioFuncionario = new JMenuItem("Funcionários");
+	private JMenuItem menuItemRelatorioAluno = new JMenuItem("Alunos");
+	private JMenuItem menuItemRelatorioEntrega = new JMenuItem("Entrega");
 
-    ImageIcon icon = new ImageIcon("imagens/leituras.png");
-    JLabel label = new JLabel(icon);
-    
-    public Principal() {
-        super("Sistema de Controle v2.0 - 211 - ETEC Zona Leste");
+	private JMenuItem menuItemSobre = new JMenuItem("Sobre");
+	private JMenuItem menuItemFechar = new JMenuItem("Fechar");
 
-        //adicona os JMenuItem no JMenu
-        menuCadastrar.add(menuItemCadastrarAluno);
-        //menuCadastrar.add(menuItemCadastrarBiometria);
-        menuCadastrar.add(menuItemCadastrarFuncionario);
-        menuCadastrar.add(menuItemCadastrarRefeicaoManual);
-        menuCadastrar.add(menuItemSobre);
-        menuCadastrar.addSeparator();
-        menuCadastrar.add(menuItemFechar);
+	private JMenuItem menuItemEntrega = new JMenuItem("Merenda Escolar");
 
-        menuRelatorio.add(menuItemRelatorioAluno);
-        menuRelatorio.add(menuItemRelatorioFuncionario);
-        menuRelatorio.add(menuItemRelatorioEntrega);
+	private JMenuBar menuBar = new JMenuBar();
 
-        menuEntrega.add(menuItemEntrega);
+	ImageIcon icon = new ImageIcon("imagens/leituras.png");
+	JLabel label = new JLabel(icon);
 
-        //adiciona o JMenu no JMenuBar
-        menuBar.add(menuCadastrar);
-        menuBar.add(menuRelatorio);
-        menuBar.add(menuEntrega);
+	public Principal() {
+		super("Sistema de Controle v2.0 - 211 - ETEC Zona Leste");
 
-        this.setJMenuBar(menuBar);
+		// adicona os JMenuItem no JMenu
+		menuCadastrar.add(menuItemCadastrarAluno);
+		// menuCadastrar.add(menuItemCadastrarBiometria);
+		menuCadastrar.add(menuItemCadastrarFuncionario);
+		menuCadastrar.add(menuItemCadastrarRefeicaoManual);
+		menuCadastrar.add(menuItemSobre);
+		menuCadastrar.addSeparator();
+		menuCadastrar.add(menuItemFechar);
 
-        menuItemEntrega.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {                    
-                    //Alterar
-                    //Runtime.getRuntime().exec("C:\\biometria\\EnrollmentSample CS.exe");
-                	MainForm main = new MainForm();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
+		menuRelatorio.add(menuItemRelatorioAluno);
+		menuRelatorio.add(menuItemRelatorioFuncionario);
+		menuRelatorio.add(menuItemRelatorioEntrega);
 
-        menuItemCadastrarBiometria.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    AlunosManager alunosManager = new AlunosManager();
-                    Alunos aluno = new Alunos();
-                    
-                    int rm = Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do Aluno a ser cadastrado"));
-                    System.out.println(rm);
-                    aluno = alunosManager.findByRm(rm);
-                    
-                    JOptionPane.showMessageDialog(null, "Aluno(a) cadastrado(a) na tabela verifica");
-                    
-                    //Alterar
-                    //Runtime.getRuntime().exec("cmd.exe cd C:\\GrFinger\\Captura\\bin\\ &  /c 'start captura.exe'");
-                    //Runtime.getRuntime().exec("c:\\GrFinger\\Captura\\bin\\captura.exe");
-                    
-                } catch (Throwable ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-        
-        menuItemCadastrarRefeicaoManual.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                LancamentoRefeicoesManuais lancamento = new LancamentoRefeicoesManuais();
-            }
-        });
+		menuEntrega.add(menuItemEntrega);
 
-        menuItemRelatorioEntrega.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                RelatorioEntregas relatorioEntrega = new RelatorioEntregas();
-            }
-        });
+		// adiciona o JMenu no JMenuBar
+		menuBar.add(menuCadastrar);
+		menuBar.add(menuRelatorio);
+		menuBar.add(menuEntrega);
 
-        menuItemRelatorioAluno.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                RelatorioAlunos relatorioAlunos = new RelatorioAlunos();
-            }
-        });
+		this.setJMenuBar(menuBar);
 
-        menuItemRelatorioFuncionario.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                RelatorioFuncionarios relatorioFuncionarios = new RelatorioFuncionarios();
-            }
-        });
+		menuItemEntrega.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					// Alterar
+					// Runtime.getRuntime().exec("C:\\biometria\\EnrollmentSample CS.exe");
+					MainForm main = new MainForm();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 
-        menuItemCadastrarAluno.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                CadastrarAlunos cadastrarAlunos = new CadastrarAlunos();
-            }
-        });
+		menuItemCadastrarBiometria.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					AlunosManager alunosManager = new AlunosManager();
+					Alunos aluno = new Alunos();
 
-        menuItemCadastrarFuncionario.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                CadastrarFuncionarios cadastrarFuncionarios = new CadastrarFuncionarios();
-            }
-        });
+					int rm = Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do Aluno a ser cadastrado"));
+					System.out.println(rm);
+					aluno = alunosManager.findByRm(rm);
 
-        menuItemSobre.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Sobre sobre = new Sobre();
-            }
-        });
+					JOptionPane.showMessageDialog(null, "Aluno(a) cadastrado(a) na tabela verifica");
 
-        menuItemFechar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon icon = new ImageIcon("imagens\\icones-bio.gif");
-                int i = JOptionPane.showConfirmDialog(null, "Você deseja encerrar essa aplicação?", "Sistema de Controle v2.0", JOptionPane.YES_NO_OPTION, 1, null);
-                if (i == 0) {
-                    System.exit(0);
-                }
-            }
-        });
+					// Alterar
+					// Runtime.getRuntime().exec("cmd.exe cd C:\\GrFinger\\Captura\\bin\\ & /c
+					// 'start captura.exe'");
+					// Runtime.getRuntime().exec("c:\\GrFinger\\Captura\\bin\\captura.exe");
 
-        Container paine = this.getContentPane();
-        paine.setLayout(new BorderLayout());
+				} catch (Throwable ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 
-        paine.add(label, BorderLayout.CENTER);
-        //label.setBounds(350, 150, 668, 503);
- 
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage("imagens\\icones-bio.gif"));
-        this.setSize(Toolkit.getDefaultToolkit().getScreenSize().getSize());
-        this.setVisible(true);
-        this.setResizable(false);
-        //this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-    }
-    public static void main(String args[]){
-        Principal principal = new Principal();
-    }
+		menuItemCadastrarRefeicaoManual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LancamentoRefeicoesManuais lancamento = new LancamentoRefeicoesManuais();
+			}
+		});
+
+		menuItemRelatorioEntrega.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RelatorioEntregas relatorioEntrega = new RelatorioEntregas();
+			}
+		});
+
+		menuItemRelatorioAluno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RelatorioAlunos relatorioAlunos = new RelatorioAlunos();
+			}
+		});
+
+		menuItemRelatorioFuncionario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RelatorioFuncionarios relatorioFuncionarios = new RelatorioFuncionarios();
+			}
+		});
+
+		menuItemCadastrarAluno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CadastrarAlunos cadastrarAlunos = new CadastrarAlunos();
+			}
+		});
+
+		menuItemCadastrarFuncionario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CadastrarFuncionarios cadastrarFuncionarios = new CadastrarFuncionarios();
+			}
+		});
+
+		menuItemSobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sobre = new Sobre();
+			}
+		});
+
+		menuItemFechar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ImageIcon icon = new ImageIcon("imagens\\icones-bio.gif");
+				int i = JOptionPane.showConfirmDialog(null, "Você deseja encerrar essa aplicação?",
+						"Sistema de Controle v2.0", JOptionPane.YES_NO_OPTION, 1, null);
+				if (i == 0) {
+					System.exit(0);
+				}
+			}
+		});
+
+		Container paine = this.getContentPane();
+		paine.setLayout(new BorderLayout());
+
+		paine.add(label, BorderLayout.CENTER);
+		// label.setBounds(350, 150, 668, 503);
+
+		this.setIconImage(Toolkit.getDefaultToolkit().getImage("imagens\\icones-bio.gif"));
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize().getSize());
+		this.setVisible(true);
+		this.setResizable(false);
+		// this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	}
+
+	private static FileReader criaReaderDoArquivoDeEntregasDoDia() {
+		FileReader fileReader = null;
+		try {			
+			fileReader = new FileReader(Principal.arqDeEntregasDoDia);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return fileReader;
+	}
+
+	private static File criaArquivoDeEntregasDoDia() {
+		String nomeDoArqDeEntregas = "EntregasDeHoje_" + formatadorDeData.format(new Date()) + ".txt";
+		return new File(nomeDoArqDeEntregas);
+	}
+
+	private static PrintStream escritorDeLinhaDoArquivoDeEntregas() {
+		PrintStream printStream = null;
+		try {
+			//printStream = new PrintStream(Principal.arqDeEntregasDoDia);
+			printStream = new PrintStream(
+					new BufferedOutputStream(
+							new FileOutputStream(Principal.arqDeEntregasDoDia, true)));
+			Path path = Principal.arqDeEntregasDoDia.toPath();
+			boolean fe = Principal.arqDeEntregasDoDia.exists();
+			return printStream;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String args[]) {
+		Principal principal = new Principal();
+		Entregas entregas = new Entregas();
+		entregas.setData_retirada(new Date());
+		entregas.setHora_retirada(new Date());
+		Alunos aluno = new Alunos();
+		aluno.setRa(1234);
+		entregas.setRa(aluno);
+		registraEntregaNoArquivoDeEntregasDoDia(entregas);
+		registraEntregaNoArquivoDeEntregasDoDia(entregas);
+		registraEntregaNoArquivoDeEntregasDoDia(entregas);
+		registraEntregaNoArquivoDeEntregasDoDia(entregas);
+	}
+
+	public static void registraEntregaNoArquivoDeEntregasDoDia(Entregas entrega) {
+		String sep = ";";
+		String ra = entrega.getRa().getRa().toString();
+		String h = formatadorDeHoras.format(entrega.getHora_retirada());
+		String registro = ra + sep + h;
+		try {			
+			fileStream4ArqDeEntregasDoDia.println(registro);
+			fileStream4ArqDeEntregasDoDia.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
